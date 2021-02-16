a82d78a918915310e9cae0fb50039b8d8b6b2cf2
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-02-16 10:04:14 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ input.onButtonPressed(Button.A, function () {
     lightsOn()
 })
 function lightsOn () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     while (true) {
         for (let index = 0; index <= 30; index++) {
             strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
-            if (index >= 15) {
-                strip.setPixelColor(index % 15, neopixel.colors(NeoPixelColors.Red))
+            if (index < 15) {
+                strip.setPixelColor(index % 15 + 15, neopixel.colors(NeoPixelColors.Blue))
+            } else {
+                strip.setPixelColor(index % 15 + 0, neopixel.colors(NeoPixelColors.Blue))
             }
             strip.show()
             basic.pause(100)

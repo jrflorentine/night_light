c8c7821a123c12ce17fd421f1f23f4ebcccb8961
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-02-16 11:02:07 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,27 @@
-input.onButtonPressed(Button.A, function () {
-    lightsOn()
-})
-function lightsOn () {
-    while (true) {
-        for (let index = 0; index <= 30; index++) {
-            strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
-            if (index < 15) {
-                strip.setPixelColor(index % 15 + 15, neopixel.colors(NeoPixelColors.Blue))
-            } else {
-                strip.setPixelColor(index % 15 + 0, neopixel.colors(NeoPixelColors.Blue))
-            }
-            strip.show()
-            basic.pause(100)
+function lightsOn (bool: boolean) {
+    for (let index = 0; index <= 30; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
+        if (index < 15) {
+            strip.setPixelColor(index % 15 + 15, neopixel.colors(NeoPixelColors.Blue))
+        } else {
+            strip.setPixelColor(index % 15 + 0, neopixel.colors(NeoPixelColors.Blue))
         }
+        strip.show()
+        basic.pause(100)
     }
+    return
 }
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+basic.forever(function () {
+    while (input.lightLevel() < 50) {
+        lightsOn(true)
+        servos.P1.run(5)
+    }
+    while (input.buttonIsPressed(Button.A)) {
+        servos.P1.run(5)
+        lightsOn(true)
+    }
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    servos.P1.stop()
+})

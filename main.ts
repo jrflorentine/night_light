4d3c08a7f81931f7c260311a1cc1b5bceab7a4f8
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-03-01 17:21:19 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 function lightsOn (bool: boolean) {
     for (let index = 0; index <= 30; index++) {
-        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Orange))
         if (index < 15) {
             strip.setPixelColor(index % 15 + 15, neopixel.colors(NeoPixelColors.Blue))
         } else {
@@ -14,9 +14,9 @@ function lightsOn (bool: boolean) {
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 basic.forever(function () {
-    while (input.lightLevel() < 50) {
-        lightsOn(true)
+    while (input.lightLevel() < 5) {
         servos.P1.run(5)
+        lightsOn(true)
     }
     while (input.buttonIsPressed(Button.A)) {
         servos.P1.run(5)
